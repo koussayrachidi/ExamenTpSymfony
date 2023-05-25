13e49bd1afe4d50cbbb9d6debda1d7b72b023818
Koussay Rachidi <koussayrachidi2@gmail.com>
2023-05-25 11:27:20 +0100

    first commit

---
+++ src/Entity/Article.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Contenu = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $DateDePublication = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->Contenu;
+    }
+
+    public function setContenu(string $Contenu): self
+    {
+        $this->Contenu = $Contenu;
+
+        return $this;
+    }
+
+    public function getDateDePublication(): ?\DateTimeInterface
+    {
+        return $this->DateDePublication;
+    }
+
+    public function setDateDePublication(\DateTimeInterface $DateDePublication): self
+    {
+        $this->DateDePublication = $DateDePublication;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $MotDePasse = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->MotDePasse;
+    }
+
+    public function setMotDePasse(string $MotDePasse): self
+    {
+        $this->MotDePasse = $MotDePasse;
+
+        return $this;
+    }
+}
